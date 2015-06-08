d3b4ede4d3e061f4f1f6945da546256f016ecf59
Kirk Chen <rwk0119@gmail.com>
2015-06-08 09:47:45 +0800

    Rewrite postlist logic with flux

---
+++ app/actions/postlistAction.js
@@ -0,0 +1,13 @@
+// Flux
+var Dispatcher = require('../dispatchers/appDispatcher');
+
+// Actions
+var postlistActions = {
+    getAllPosts: function(){
+        Dispatcher.handleViewAction({
+            type: 'getAllPosts'
+        });
+    }
+};
+
+module.exports = postlistActions;
