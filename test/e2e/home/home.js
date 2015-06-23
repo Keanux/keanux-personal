d47e330346dbbd3a998da6e5da0365324bb9b215
Kirk Chen <kirkchen@nine-yi.com>
2015-06-23 09:26:22 +0800

    fix jscs style error

--- test/e2e/home/home.js
+++ test/e2e/home/home.js
@@ -3,27 +3,27 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-describe('HomePage', function () {
-  beforeEach(function (done) {
+describe('HomePage', function() {
+  beforeEach(function(done) {
     browser.get('/');
 
     // Hack for wait react initial
     browser.sleep(1 * 1000).then(done);
   });
 
-  it('Title should be "Keanux-Personal Demo"', function (done) {
+  it('Title should be "Keanux-Personal Demo"', function(done) {
     browser.getTitle()
-      .then(function (title) {
+      .then(function(title) {
         expect(title).to.equal('Keanux-Personal Demo');
 
         done();
       });
   });
 
-  it('Article\'s count should be 3', function (done) {
+  it('Article\'s count should be 3', function(done) {
     element.all(by.css('.post'))
       .count()
-      .then(function (count) {
+      .then(function(count) {
         expect(count).to.equal(3);
 
         done();
